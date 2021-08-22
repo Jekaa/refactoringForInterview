072bdcd89039ecfd304ae7c41ced941b25581a40
rakovi4 <vasyanyapupkin@gmail.com>
2021-08-23 01:56:23 +0300

    first extract

--- src/main/java/example/Customer.java
+++ src/main/java/example/Customer.java
@@ -22,21 +22,7 @@ class Customer {
         int frequentRenterPoints = 0;
         String result = "Rental Record for " + getName() + "\n";
         for (Rental each : rentals) {
-            double thisAmount = 0;
-            //determine amounts for each line
-            switch (each.getMovie().getPriceCode()) {
-                case Movie.REGULAR -> {
-                    thisAmount += 2;
-                    if (each.getDaysRented() > 2)
-                        thisAmount += (each.getDaysRented() - 2) * 1.5;
-                }
-                case Movie.NEW_RELEASE -> thisAmount += each.getDaysRented() * 3;
-                case Movie.CHILDRENS -> {
-                    thisAmount += 1.5;
-                    if (each.getDaysRented() > 3)
-                        thisAmount += (each.getDaysRented() - 3) * 1.5;
-                }
-            }
+            double thisAmount = getCharge(each);
             // add frequent renter points
             frequentRenterPoints ++;
             // add bonus for a two day new release rental
@@ -50,6 +36,25 @@ class Customer {
         result += "Amount owed is " + totalAmount + "\n";
         result += "You earned " + frequentRenterPoints + " frequent renter points";
         return result;
+    }
+
+    private double getCharge(Rental rental) {
+        double charge = 0;
+        //determine amounts for rental line
+        switch (rental.getMovie().getPriceCode()) {
+            case Movie.REGULAR -> {
+                charge += 2;
+                if (rental.getDaysRented() > 2)
+                    charge += (rental.getDaysRented() - 2) * 1.5;
+            }
+            case Movie.NEW_RELEASE -> charge += rental.getDaysRented() * 3;
+            case Movie.CHILDRENS -> {
+                charge += 1.5;
+                if (rental.getDaysRented() > 3)
+                    charge += (rental.getDaysRented() - 3) * 1.5;
+            }
+        }
+        return charge;
     }
 
 
