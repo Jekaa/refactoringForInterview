e96969e40b9f8080ea58c5529b96cd7584f860bd
rakovi4 <vasyanyapupkin@gmail.com>
2021-08-22 17:00:26 +0300

    last preparation

--- src/main/java/example/Customer.java
+++ src/main/java/example/Customer.java
@@ -2,6 +2,7 @@ package example;
 
 import java.util.List;
 
+@SuppressWarnings("StringConcatenationInLoop")
 class Customer {
     private final String name;
     private final List<Rental> rentals;
@@ -39,20 +40,15 @@ class Customer {
             // add frequent renter points
             frequentRenterPoints ++;
             // add bonus for a two day new release rental
-            if ((each.getMovie().getPriceCode() == Movie.NEW_RELEASE)
-                    &&
-                    each.getDaysRented() > 1) frequentRenterPoints ++;
+            if ((each.getMovie().getPriceCode() == Movie.NEW_RELEASE) && each.getDaysRented() > 1)
+                frequentRenterPoints ++;
             //show figures for this rental
-            result += "\t" + each.getMovie().getTitle()+ "\t" +
-                    String.valueOf(thisAmount) + "\n";
+            result += "\t" + each.getMovie().getTitle()+ "\t" + thisAmount + "\n";
             totalAmount += thisAmount;
         }
         //add footer lines
-        result += "Amount owed is " + String.valueOf(totalAmount) +
-                "\n";
-        result += "You earned " + String.valueOf(frequentRenterPoints)
-                +
-                " frequent renter points";
+        result += "Amount owed is " + totalAmount + "\n";
+        result += "You earned " + frequentRenterPoints + " frequent renter points";
         return result;
     }
 
