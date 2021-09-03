11419823e1283b51283f4892295e6a4eeb6244c9
rakovi4 <vasyanyapupkin@gmail.com>
2021-09-03 15:45:53 +0300

    Overridden getCharge in ChildrensPrice class

--- src/main/java/example/ChildrensPrice.java
+++ src/main/java/example/ChildrensPrice.java
@@ -6,4 +6,12 @@ public class ChildrensPrice extends Price {
     public ChildrensPrice() {
         super(CHILDRENS);
     }
+
+    @Override
+    double getCharge(int daysRented) {
+        if (daysRented > 3) {
+            return 1.5 + (daysRented - 3) * 1.5;
+        }
+        return 1.5;
+    }
 }
