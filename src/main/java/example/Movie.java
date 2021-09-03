ff21f9e3ddbbf625d51e8be8484ca4daadb8c3c5
rakovi4 <vasyanyapupkin@gmail.com>
2021-09-03 15:56:37 +0300

    Removed Conditional constants

--- src/main/java/example/Movie.java
+++ src/main/java/example/Movie.java
@@ -1,24 +1,12 @@
 package example;
 
 public class Movie {
-    public static final int CHILDRENS = 2;
-    public static final int REGULAR = 0;
-    public static final int NEW_RELEASE = 1;
-
-
     private final String title;
     private final Price price;
 
     public Movie(String title, Price price) {
         this.title = title;
         this.price = price;
-    }
-
-    public Price createPrice(int priceCode) {
-        if (priceCode == REGULAR) return new RegularPrice();
-        if (priceCode == CHILDRENS) return new ChildrensPrice();
-        if (priceCode == NEW_RELEASE) return new NewReleasePrice();
-        throw new RuntimeException("No priceCode found " + priceCode);
     }
 
     public String getTitle (){
