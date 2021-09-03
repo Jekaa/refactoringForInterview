efefe84ea444466fe9fc1bbd9a267ad06ae0abf4
rakovi4 <vasyanyapupkin@gmail.com>
2021-09-03 15:34:46 +0300

    Moved renterPoints calc to Movie

--- src/main/java/example/Movie.java
+++ src/main/java/example/Movie.java
@@ -39,4 +39,14 @@ public class Movie {
         }
         return charge;
     }
+
+    int frequentPoints(int daysRented) {
+        // add frequent renter points
+        int frequentRenterPoints = 0;
+        frequentRenterPoints++;
+        // add bonus for a two day new release rental
+        if ((getPriceCode() == NEW_RELEASE) && daysRented > 1)
+            frequentRenterPoints++;
+        return frequentRenterPoints;
+    }
 }
--- src/main/java/example/Rental.java
+++ src/main/java/example/Rental.java
@@ -22,12 +22,7 @@ class Rental {
     }
 
     int getFrequentRenterPointsIncrement() {
-        // add frequent renter points
-        int frequentRenterPoints = 0;
-        frequentRenterPoints++;
-        // add bonus for a two day new release rental
-        if ((getMovie().getPriceCode() == Movie.NEW_RELEASE) && getDaysRented() > 1)
-            frequentRenterPoints++;
-        return frequentRenterPoints;
+        return getMovie().frequentPoints(getDaysRented());
     }
+
 }
